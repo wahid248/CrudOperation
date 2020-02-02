7a78a3936e576fd8a436b0e5aa53bea82ad41fd0
Wahid <wahid.cseaust@gmail.com>
2020-02-02 20:19:50 +0600

    add controller

---
+++ Api/Controllers/PlansController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlansController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult GetPlan()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public IActionResult SavePlan()
+        {
+            return Ok();
+        }
+    }
+}
